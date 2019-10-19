547b010507a402ce1e7a09d43b5e9ed8bb0583bd
Dyumin <jejni.j@gmail.com>
2019-10-20 00:33:57 +0300

    Remove boilerplate code

---
+++ inc/watchgram.h
@@ -0,0 +1,19 @@
+#ifndef __watchgram_H__
+#define __watchgram_H__
+
+#include <app.h>
+#include <Elementary.h>
+#include <system_settings.h>
+#include <efl_extension.h>
+#include <dlog.h>
+
+#ifdef  LOG_TAG
+#undef  LOG_TAG
+#endif
+#define LOG_TAG "watchgram"
+
+#if !defined(PACKAGE)
+#define PACKAGE "com.dyumin.watchgram"
+#endif
+
+#endif /* __watchgram_H__ */
